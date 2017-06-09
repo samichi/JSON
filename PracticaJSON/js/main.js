12066b4721b26b69b261edcb0df8c194b484ae08
samichi <samichi@users.noreply.github.com>
2017-06-08 19:12:50 -0500

    JSON
    Practica finalizada

---
+++ PracticaJSON/js/main.js
@@ -0,0 +1,38 @@
+function miFuncion(){
+  var myObj = JSON.parse(' [ {"Name" : "Alfreds Futterkiste", "City" : "Berlin", "Country" : "Germany" }, { "Name" : "Berglunds snabbköp", "City" : "Luleå", "Country" : "Sweden" }, { "Name" : "Centro comercial Moctezuma", "City" : "México D.F.", "Country" : "Mexico" }, { "Name" : "Ernst Handel", "City" : "Graz", "Country" : "Austria" }, { "Name" : "FISSA Fabrica Inter. Salchichas S.A.", "City" : "Madrid", "Country" : "Spain" }, { "Name" : "Galería del gastrónomo", "City" : "Barcelona", "Country" : "Spain" }, { "Name" : "Island Trading", "City" : "Cowes", "Country" : "UK" }, { "Name" : "Königlich Essen", "City" : "Brandenburg", "Country" : "Germany" }, { "Name" : "Laughing Bacchus Wine Cellars", "City" : "Vancouver", "Country" : "Canada" }, { "Name" : "Magazzini Alimentari Riuniti", "City" : "Bergamo", "Country" : "Italy" }, { "Name" : "North/South", "City" : "London", "Country" : "UK" }, { "Name" : "Paris spécialités", "City" : "Paris", "Country" : "France" }, { "Name" : "Rattlesnake Canyon Grocery", "City" : "Albuquerque", "Country" : "USA" }, { "Name" : "Simons bistro", "City" : "København", "Country" : "Denmark" }, { "Name" : "The Big Cheese", "City" : "Portland", "Country" : "USA" }, { "Name" : "Vaffeljernet", "City" : "Århus", "Country" : "Denmark" }, { "Name" : "Wolski Zajazd", "City" : "Warszawa", "Country" : "Poland"  } ]');
+
+
+  function compare(a, b) {
+    if (a.City < b.City)
+      return 1;
+    if (a.City > b.City)
+      return -1;
+    return 0;
+  }
+
+  myObj.sort(compare);
+
+  var table = document.getElementById("demoTabla");
+
+
+    for (i = 0; i < myObj.length; i++) {
+        
+        var row = table.insertRow(0);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        //x += 
+        cell1.innerHTML = myObj[i].City;
+        cell2.innerHTML = myObj[i].Country;
+        cell3.innerHTML = myObj[i].Name;
+        //document.getElementById("demo").innerHTML = obj[i];
+
+        //document.getElementById("demo").innerHTML = obj[i].Name + " ";
+
+    }
+ 
+}
+
+window.onload = function () { 
+    miFuncion();
+}
